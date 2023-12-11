8ad8df17d48d0c43aca2d70cc5638dc2a022344b
Alex Charos <realtowz@yahoo.com>
2023-12-11 20:20:33 +0200

    day 9 part one uncleaned

---
+++ src/main/java/gr/charos/christmas/day9/ExerciseOne.java
@@ -0,0 +1,55 @@
+package gr.charos.christmas.day9;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import gr.charos.christmas.Utils;
+
+public class ExerciseOne {
+	
+	public static void main(String args[]) {
+		List<String> lines =Utils.loadLines(ExerciseOne.class);
+		
+		List<Integer> sums = new ArrayList<Integer>();
+		for (String s : lines) {
+			
+			List<Integer>  list = Arrays.asList( s.split(" ")).stream().filter(p->p.length()>0).map(Integer::parseInt).collect(Collectors.toList());
+			
+			
+			List<List<Integer>> history = new ArrayList<List<Integer>>();
+			while (!list.stream().allMatch(p->p==0)) {
+				history.add(list);
+				list = calculateDiffs(list);
+			}
+			
+			int toAddBelow = 0;
+			for (int i = history.size()-1; i >=0; i--) {
+				List<Integer> comp = history.get(i);
+				int toAdd = comp.get(comp.size()-1);
+				int tmp = toAdd+toAddBelow;
+				comp.add(tmp);
+				toAddBelow = tmp;
+			}
+			
+			
+			sums.add(toAddBelow);
+			
+		}
+		System.out.println(sums.stream().mapToInt(Integer::valueOf).sum());
+		
+		
+		
+	}
+	
+
+	static List<Integer> calculateDiffs(List<Integer> list) {
+		List<Integer> diffs = new ArrayList<Integer>();
+		for (int i = 0; i < list.size()-1; i++) {
+			diffs.add(list.get(i+1) - list.get(i));
+		}
+		return diffs;
+	}
+
+}
